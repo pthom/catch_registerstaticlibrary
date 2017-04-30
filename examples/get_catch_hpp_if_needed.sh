86d1580d7bf9eab72ae7c24c967fa449bf07d3cc
Pascal Thomet <pthomet@ivsweb.com>
2017-04-30 17:41:05 +0200

    Add a checkout of Catch v1.9.3

--- examples/get_catch_hpp_if_needed.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/sh
-if [ ! -f catch/catch.hpp ]; then
-  echo "getting latest catch.hpp version into examples/catch/catch.hpp"
-  mkdir catch
-  wget https://raw.githubusercontent.com/philsquared/Catch/master/single_include/catch.hpp -O catch/catch.hpp
-else
-  echo "examples/catch/catch.hpp is already here"
-fi
