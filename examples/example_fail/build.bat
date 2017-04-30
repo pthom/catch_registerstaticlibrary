b97368fbc3719b2b3047e209b6043b2fd33bc385
Pascal Thomet <pthomet@ivsweb.com>
2017-04-30 17:40:03 +0200

    example_fail now also compiles under windows

---
+++ examples/example_fail/build.bat
@@ -0,0 +1,8 @@
+
+deltree /Y build
+mkdir build
+cd build
+cmake ..
+cmake --build .
+.\bin\Debug\MyLibraryTest.exe
+cd ..
--- examples/example_fail/build.sh
+++ examples/example_fail/build.sh
@@ -1,9 +1,5 @@
 #!/usr/bin/env bash
 
-cd ..
-./get_catch_hpp_if_needed.sh
-cd -
-
 rm -rf build
 mkdir build
 cd build
