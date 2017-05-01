399a99dba5bba8e40d07e5e708621205ff6cb35e
Pascal Thomet <pthomet@ivsweb.com>
2017-05-01 18:13:58 +0200

    undo process (before redo)

--- examples/example_register_processed/MyLibrary/lib1.cpp
+++ examples/example_register_processed/MyLibrary/lib1.cpp
@@ -9,8 +9,3 @@ TEST_CASE("testing the factorial function") {
   CHECK(factorial(2) == 2);
   CHECK(factorial(10) == 3628800);
 }
-
-// This code was added by catch_registerstaticlibrary.cmake in order to ensure that the tests are properly run by catch.
-// Please commit it if needed, it will be added only once, and never modified.
-// Before committing, you can remove this comment, as long as you leave the function below.
-int catchRegister_781f5131_2d32_4cd8_92cf_d83b7f4fcd20() { return 0; }
--- examples/example_register_processed/MyLibrary/lib2.cpp
+++ examples/example_register_processed/MyLibrary/lib2.cpp
@@ -6,8 +6,3 @@ TEST_CASE("testing in lib2.cpp") {
     std::cout << "testing in lib2.cpp" << std::endl;
     CHECK((1+1) == 2);
 }
-
-// This code was added by catch_registerstaticlibrary.cmake in order to ensure that the tests are properly run by catch.
-// Please commit it if needed, it will be added only once, and never modified.
-// Before committing, you can remove this comment, as long as you leave the function below.
-int catchRegister_d040fbb6_21b9_4865_8de0_c05702d9ae1e() { return 0; }
