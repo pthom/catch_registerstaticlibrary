7e0ba6bc5d5fe1d178f57317716e915278c34562
Pascal Thomet <pthomet@ivsweb.com>
2017-05-01 18:14:58 +0200

    Full diff of static library registration (automatically generated code during build) This code is generated only once.

--- examples/example_register_processed/MyLibrary/lib1.cpp
+++ examples/example_register_processed/MyLibrary/lib1.cpp
@@ -9,3 +9,8 @@ TEST_CASE("testing the factorial function") {
   CHECK(factorial(2) == 2);
   CHECK(factorial(10) == 3628800);
 }
+
+// This code was added by rsl_registerstaticlibrary.cmake in order to ensure that the tests are properly run.
+// Please commit it if needed, it will be added only once, and never modified.
+// Before committing, you can remove this comment, as long as you leave the function below.
+int RslRegister_c774c3c1_ae12_4c4c_9faa_93c8a7140a5c() { return 0; }
--- examples/example_register_processed/MyLibrary/lib2.cpp
+++ examples/example_register_processed/MyLibrary/lib2.cpp
@@ -6,3 +6,8 @@ TEST_CASE("testing in lib2.cpp") {
     std::cout << "testing in lib2.cpp" << std::endl;
     CHECK((1+1) == 2);
 }
+
+// This code was added by rsl_registerstaticlibrary.cmake in order to ensure that the tests are properly run.
+// Please commit it if needed, it will be added only once, and never modified.
+// Before committing, you can remove this comment, as long as you leave the function below.
+int RslRegister_585e2fbf_a17b_46a2_887a_093a38c17173() { return 0; }
